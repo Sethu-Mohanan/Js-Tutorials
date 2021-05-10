5d93cc543f169454c0387b03a99ea0e6bcbf801c
Sethu Mohanan <sethumohanan8@gmail.com>
2021-05-10 13:29:07 +0530

    complete

---
+++ js-basics/js/app.js
@@ -0,0 +1,33 @@
+async function getUsers(){
+    let url='users.json'
+
+    try{
+        let res=await fetch(url)
+        return await res.json();
+    }catch(error){
+        console.log(error);
+    }
+
+}
+
+async function renderUsers(){
+    let users = await getUsers();
+    let html = '';
+
+    users.forEach(user => {
+        let htmlSegment = 
+        `<div class="user">
+            <img src="&{user.profileURL}"></img>
+            <h2>${user.firstName} ${user.lastName}</h2>
+            <div class="email"><a href="email:${user.email}">${user.email}</a>
+            </div>
+        
+        `;
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
+
+renderUsers();
